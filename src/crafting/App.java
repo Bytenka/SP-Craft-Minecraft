c0da58317c12ed0abf2cda90fb8c64bac63b6aa2
Bytenka <slybytenka@gmail.com>
2019-02-28 18:06:16 +0100

    Switched to the Swing API

--- src/crafting/App.java
+++ src/crafting/App.java
@@ -1,66 +1,58 @@
 package crafting;
 
 import java.awt.Color;
-import java.awt.Frame;
-import java.awt.Toolkit;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.Graphics;
+import java.io.File;
+import java.io.IOException;
 
-public class App extends Frame implements WindowListener {
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class App extends JFrame {
+	public static final int DEFAULT_WIDTH = 500;
+	public static final int DEFAULT_HEIGHT = 500;
+
+	private String iconPath = "img/icon.png";
+	private InnerPanel panel;
 
 	public App() {
-		this.setSize(500, 500);
-		this.setBackground(Color.gray);
+		panel = new InnerPanel();
+
+		// Initializing frame
+		try {
+			this.setIconImage(ImageIO.read(new File(iconPath)));
+		} catch (IOException e) {
+			System.err.println("Unable to set app icon: " + iconPath + ": file not found!");
+		}
+
 		this.setTitle("Crafting bench");
-		this.setIconImage(Toolkit.getDefaultToolkit().getImage("img/icon.png"));
-		
-		this.addWindowListener(this);
-		
+		this.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		this.getContentPane().setBackground(Color.black); // Color in case of errors
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE); // Just to be explicit
+
+		this.add(panel);
+
 		this.setVisible(true);
+	}
+	
+	// Inner class to have something to draw to
+	private class InnerPanel extends JPanel {
+		public InnerPanel() {
+			// Initializing panel
+			this.setBackground(Color.cyan);
+			this.setVisible(true);
+		}
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.setColor(Color.blue);
+			g.fillRect(10,  10, 50, 50);
+		}
 	}
 
 	public static void main(String[] args) {
 		new App();
-	}
-
-	@Override
-	public void windowClosing(WindowEvent arg0) {
-		this.dispose();
-	}
-
-	@Override
-	public void windowActivated(WindowEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void windowClosed(WindowEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void windowDeactivated(WindowEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void windowDeiconified(WindowEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void windowIconified(WindowEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void windowOpened(WindowEvent arg0) {
-		// TODO Auto-generated method stub
-
 	}
 }
