7f6fd9ad44b1bbca0a182a9f1039168434eed313
Francois Miramond <fmiramon@pcalb-mm0000.univ-jfc.fr>
2019-03-19 12:06:27 +0100

    debut de la creation de la DB

---
+++ src/crafting/Craft.java
@@ -0,0 +1,11 @@
+package crafting;
+
+public class Craft {
+
+	Item[][] tab;
+	
+	public Craft (Item[][] Item) {
+		// sTODO shrink
+		this.tab = null;
+	}
+}
---
+++ src/crafting/CraftDB.java
@@ -0,0 +1,9 @@
+package crafting;
+
+public final class CraftDB {
+
+	public static Item craftExists(Craft combi) {
+		// TODO check if craft is in the json
+		return null;
+	}
+}
